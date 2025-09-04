e5182f23e98f552e94c5591f209619ba5220ae76
Steffen1508 <II70PC@student.aau.dk>
2025-09-04 14:49:35 +0200

    lektion1

--- Lektion1/draw.py
+++ Lektion1/draw.py
@@ -4,13 +4,22 @@ pygame.init() # Initialize Pygame
 screen = pygame.display.set_mode((640, 480)) # Create a window of 640x480 pixels
 screen.fill((255, 255, 255)) # Fill the screen with white
 
-pygame.draw.line(screen, (0, 0, 0), (100, 380), (540, 380)) # Draw a black line
+
+
 
 # Draw the ground
-# Draw the bottom of the house
-# Draw two walls
-# Draw the roof
+pygame.draw.line(screen, (0, 255, 0), (0, 380), (1000, 380)) # Draw a green line
 
+# Draw the bottom of the house
+pygame.draw.line(screen, (0, 0, 0), (100, 375), (540, 375)) # Draw a black line
+
+# Draw two walls
+pygame.draw.line(screen, (0, 0, 0), (100, 375), (100, 100)) # Draw a black line
+pygame.draw.line(screen, (0, 0, 0), (540, 375), (540, 100)) # Draw a black line
+
+# Draw the roof
+pygame.draw.line(screen, (0, 0, 0), (100, 100), (320, 40)) # Draw a black line
+pygame.draw.line(screen, (0, 0, 0), (320, 40), (540, 100)) # Draw a black line
 # Make sure the window stays open until the user closes it
 run_flag = True
 while run_flag is True:
