0251b3aa675c49a97928a4e20e394ecd798042ef
Steffen1508 <II70PC@student.aau.dk>
2025-09-17 14:39:13 +0200

    færdig

--- IntroPython/Lektion 2/Mini projekt.py
+++ IntroPython/Lektion 2/Mini projekt.py
@@ -18,6 +18,18 @@ viser_start_point = [320, 240]
 pygame.mixer.init()
 sounds = [pygame.mixer.Sound(f"sounds/{i:02d}.mp3") for i in range(1, 61)]
 
+def draw_digital_clock(screen):
+    font = pygame.font.SysFont("Consolas", 24, bold=True)
+
+    box_rect = pygame.Rect(0, 0, 1100, 850)
+
+    time_str = datetime.now().strftime("%H:%M:%S")
+    text_surf = font.render(time_str, True, (80, 255, 120))
+    text_rect = text_surf.get_rect(center=box_rect.center)
+    screen.blit(text_surf, text_rect)
+
+last_second = datetime.now().second
+
 run_flag = True
 while run_flag:
 
@@ -25,15 +37,13 @@ while run_flag:
         if event.type == pygame.QUIT:
             run_flag = False
 
-    last_second = datetime.now().second
- 
-    screen.fill((0, 0, 0))  # ryd hele skærmen
+    # screen
+    screen.fill((0, 0, 0))
     pygame.draw.circle(screen, (255, 255, 255), start_point, 201, 1)
-    
-#Sekund takker
+
+    # Sekund takker
     length = 200
     for angle in range(0, 360, 6):
-        
         x_cord = start_point[0] + length * math.cos(math.radians(angle))
         y_cord = start_point[1] + length * math.sin(math.radians(angle))
         end_point = [x_cord, y_cord]
@@ -46,10 +56,9 @@ while run_flag:
         end_point = [x_cord, y_cord]
         pygame.draw.line(screen, (0, 0, 0), start_point, end_point, 5)
 
-#Minut takker 
+    # Minut takker
     length = 200
     for angle in range(0, 360, 30):
-        
         x_cord = start_point[0] + length * math.cos(math.radians(angle))
         y_cord = start_point[1] + length * math.sin(math.radians(angle))
         end_point = [x_cord, y_cord]
@@ -66,38 +75,36 @@ while run_flag:
     seconds = now.second
     minutes = now.minute
     hours = now.hour
-    # elapsed = time.perf_counter() - start_time
-    print(now)
-    
+
+    # Viser
     grad_sek = seconds * 6 - 90
     viser_sekund_x = viser_start_point[0] + viser_length_sekund * math.cos(math.radians(grad_sek))
     viser_sekund_y = viser_start_point[1] + viser_length_sekund * math.sin(math.radians(grad_sek))
-    pygame.draw.aaline(screen, (255, 0, 0), viser_start_point, (viser_sekund_x, viser_sekund_y), 2)
-
-
+    pygame.draw.aaline(screen, (255, 0, 0), viser_start_point, (viser_sekund_x, viser_sekund_y))
 
     grad_min = minutes * 6 - 90
     viser_minut_x = viser_start_point[0] + viser_length_minut * math.cos(math.radians(grad_min))
     viser_minut_y = viser_start_point[1] + viser_length_minut * math.sin(math.radians(grad_min))
-    pygame.draw.aaline(screen, (255, 0, 0), viser_start_point, (viser_minut_x, viser_minut_y), 2)
-    
-    grad_timer = ( (hours % 12) * 30 ) + (minutes * 0.5) - 90 #Glidende overgang mellem timer
+    pygame.draw.aaline(screen, (255, 0, 0), viser_start_point, (viser_minut_x, viser_minut_y))
+
+    grad_timer = ((hours % 12) * 30) + (minutes * 0.5) - 90
     viser_timer_x = viser_start_point[0] + viser_length_timer * math.cos(math.radians(grad_timer))
     viser_timer_y = viser_start_point[1] + viser_length_timer * math.sin(math.radians(grad_timer))
-    pygame.draw.aaline(screen, (255, 0, 0), viser_start_point, (viser_timer_x, viser_timer_y), 2)
+    pygame.draw.aaline(screen, (255, 0, 0), viser_start_point, (viser_timer_x, viser_timer_y))
 
-    pygame.draw.circle(screen,(255, 255, 255), start_point, 5)
+    pygame.draw.circle(screen, (255, 255, 255), start_point, 5)
 
-    now = datetime.now()
+    # Lyd pr. sekund
     sec = now.second
-
-    if sec != last_second:   # kun når sekundet skifter
-        sounds[sec].play()   # afspil lyden for det sekund
+    if sec != last_second:
+        sounds[sec].play()
         last_second = sec
 
+    # >>> TEGN DIGITALT UR HER <<<
+    draw_digital_clock(screen)
 
-    # --- Flip + tick ---
+    # Flip + tick
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(120)
 
 pygame.quit()
